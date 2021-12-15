d9f609404f220a5bbffa650407bba34f8023faff
Judy <jyu@lcs.on.ca>
2021-12-15 09:24:31 -0500

    Add an overlay Circle and animate it using the onAppear modifier

--- AnimationExamples/ContentView.swift
+++ AnimationExamples/ContentView.swift
@@ -12,19 +12,25 @@ struct ContentView: View {
     
     var body: some View {
         Button("Tap me") {
-            animationAmount += 1
+            //animationAmount += 1
         }
             .padding(50)
             .background(.red)
             .foregroundColor(.white)
             .clipShape(Circle())
-            .scaleEffect(animationAmount)
-            .blur(radius: (animationAmount - 1) * 3)
-            .animation(
-                .easeInOut(duration: 1)
-                    //.repeatCount(3, autoreverses: true)
-                    .repeatForever(autoreverses: true)
-                , value: animationAmount)
+            .overlay(
+                Circle()
+                    .stroke(.red)
+                    .scaleEffect(animationAmount)
+                    .opacity(2 - animationAmount)
+                    .animation(
+                        .easeInOut(duration: 1)
+                            .repeatForever(autoreverses: false)
+                        , value: animationAmount)
+            )
+            .onAppear {
+                animationAmount = 2
+            }
 
     }
 }
